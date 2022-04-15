b24146e5689086cb5ba9b7fde0853dfd81701ec7
Renan <88853399+renanpd95@users.noreply.github.com>
2022-04-15 00:20:34 +0000

    finalização do código

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
+
 name = input("Escreva nome do funcionario: ")
 sal = int(input("Qual o salário do funcionario: R$"))
 
 aum = sal*(1+0.25)
-imp = sal*(1-0.05)
+imp = aum-(1-0.05)
 
-print(
+os.system("clear")
+print("Nome do funcionario:",name)
+print("Salário Inicial: R$",sal)
+print("Salário com aumento: R$",aum)
+print("Salário final: R$",imp)
